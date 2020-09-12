07b953ba0cc390447843de00f19c303cf48ca706
Michele Buccoli <m.buccoli@gmail.com>
2020-09-12 16:50:33 +0200

    image analysis as a class

--- src/image_analysis.py
+++ src/image_analysis.py
@@ -8,44 +8,58 @@ from matplotlib import pyplot as plt
 from scipy import stats
 
 # %%
-city="milan"
 
-#root = tk.Tk()
-#root.withdraw()
+class Image_Analysis:
+    def __init__(self, name="", fn=None):
+        if fn is None:
+            root = tk.Tk()
+            root.withdraw()
+            fn = filedialog.askopenfilename()
+            name = os.path.split(fn)[-1].split(".")[0]
+        self.fn=fn
+        self.name=name
+        self.im =cv2.imread(self.fn)
+        if self.im.shape[0]>720 or  self.im.shape[1]>1280:
+            scale = min(720/self.im.shape[0], 1280/self.im.shape[1])
+            self.im = cv2.resize(self.im, (int(scale*self.im.shape[1]), int(scale*self.im.shape[0])))
+            
+        self.imgray = cv2.cvtColor(self.im,cv2.COLOR_BGR2GRAY)
+        self.ret,self.thresh = cv2.threshold(self.imgray,127,255,0)
+        
 
-#fn = filedialog.askopenfilename()
-#title = os.path.split(fn)[-1].split(".")[0]
+    def show(self,im=None):
+        if im is None:
+            cv2.imshow(self.name, self.im)
+        else:
+            cv2.imshow(self.name, im)
+        cv2.waitKey()
+    def get_stats(self):
+        means, stddevs  = cv2.meanStdDev(self.im)
+        return means, stddevs
+    def get_contours(self):
+        contours, hierarchy = cv2.findContours(self.thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        len_contours=[len(cont) for cont in contours]        
+        return contours, len_contours
 
-city_fns={"milan":"../images/milan.jpg"}
-im=cv2.imread(city_fns[city])
-#cv2.imshow(city.title(), im)
-#print("hello world")
-#cv2.waitKey()
+    def get_lines(self):   
+        edges = cv2.Canny(self.imgray,50,150,apertureSize = 3)
+        lines = cv2.HoughLines(edges,1,np.pi/180,200)
+        return lines
+
+
 # %%
-means, stddevs  = cv2.meanStdDev(im)
-print(means)
-print(stddevs)
-#ignore histogram mean and standard deviation
-color = ('b','g','r')
-mean_list = []
-std_list = []
-for i,col in enumerate(color):
-    histr = cv2.calcHist([im],[i],None,[256],[0,256])
-    mean = np.mean(histr).item()
-    std = np.std(histr).item()
-    mean_list.append(mean)
-    std_list.append(std)
-    #plt.plot(histr,color = col)
-    #plt.xlim([0,256])
+if __name__=="__main__":    
+    city_fns={"milan":"../images/milan.jpg"}
+    city="milan"
 
-#plt.show()
-imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-ret,thresh = cv2.threshold(imgray,127,255,0)
-contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-im = cv2.drawContours(im, contours, -1, (0,255,0), 3)
-print(len(contours))
-#cv2.imshow(city.title(),im)
-#cv2.waitKey()
-edges = cv2.Canny(imgray,50,150,apertureSize = 3)
-lines = cv2.HoughLines(edges,1,np.pi/180,200)
-print(len(lines))
+    #Ia= Image_Analysis(name=city.title(), fn=city_fns[city])
+    Ia= Image_Analysis(name=city.title(), fn=None)
+    means, stddevs=Ia.get_stats()
+    print(means, stddevs)
+    contours,_=Ia.get_contours()
+    lines=Ia.get_lines()
+    imcontours = cv2.drawContours(Ia.im, contours, -1, (0,255,0), 3)
+    Ia.show(imcontours)
+        
+
+# %%
