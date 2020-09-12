84e0092f200e1b8f705b9a51659839718ab4061e
Michele Buccoli <m.buccoli@gmail.com>
2020-09-12 11:51:46 +0200

    without analysis

--- src/image_analysis.py
+++ src/image_analysis.py
@@ -8,15 +8,15 @@ from tkinter import filedialog
 # %%
 city="milan"
 
-root = tk.Tk()
-root.withdraw()
+#root = tk.Tk()
+#root.withdraw()
 
-fn = filedialog.askopenfilename()
-title = os.path.split(fn)[-1].split(".")[0]
+#fn = filedialog.askopenfilename()
+#title = os.path.split(fn)[-1].split(".")[0]
 
-#city_fns={"milan":"../images/milan.jpg"}
-im=cv2.imread(fn)
-cv2.imshow(title.title(), im)
+city_fns={"milan":"../images/milan.jpg"}
+im=cv2.imread(city_fns[city])
+cv2.imshow(city.title(), im)
 print("hello world")
-#cv2.waitKey()
+cv2.waitKey()
 # %%
